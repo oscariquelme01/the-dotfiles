0d6236ed5bd25fc56e542045e5683f5d85f23b0b
oscar <oriquelme@rfranco.com>
2023-11-27 10:08:02 +0100

    mod install script

--- install.sh
+++ install.sh
@@ -7,11 +7,9 @@ if [ "$needInstall" = "y" ] || [ "$needInstall" = "Y" ]; then
 	sudo pacman -S neovim awesome zsh wezterm alacritty obsidian
 fi
 
-echo "Warning! This will overwrite any existing dotfiles from neovim, awesome, zsh, wezterm, alacritty and some of the obsidian. Press enter to continue or press CTRL-C to cancel"
-read -r
-ln -sf ~/Dotfiles/nvim ~/.config/nvim
-ln -sf ~/Dotfiles/awesome ~/.config/awesome
-ln -sf ~/Dotfiles/zsh ~/.config/zsh
-ln -sf ~/Dotfiles/wezterm ~/.config/wezterm
-ln -sf ~/Dotfiles/alacritty ~/.config/alacritty
-ln -sf ~/Dotfiles/obsidian/.obsidian.vimrc ~/.config/.obsidian.vimrc
+paths=("nvim" "awesome" "zsh" "wezterm" "alacritty" "obsidian/.obsidian.vimrc")
+
+for path in "${paths[@]}"; do
+	mv "$HOME/.config/${path}" "$HOME/.config/${path}.backup"
+	ln -sf "$HOME/Dotfiles/$path" "$HOME/.config/${path}"
+done
