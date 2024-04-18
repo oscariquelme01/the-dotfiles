52aa2d8084ee9f65e3cfc9c2fbaa29ecc57fa98d
oscar <oriquelme@rfranco.com>
2024-04-18 09:07:21 +0200

    [NVIM] fix: add missing dependencia for dap-ui

--- nvim/lua/plugins/languages/debug.lua
+++ nvim/lua/plugins/languages/debug.lua
@@ -51,6 +51,7 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
     'jay-babu/mason-nvim-dap.nvim',
     'mfussenegger/nvim-dap-python'
   },
