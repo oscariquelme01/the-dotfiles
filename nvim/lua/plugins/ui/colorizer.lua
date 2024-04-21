5430c5c8b373d1663442a49fe127e7330bd33885
oscar <oriquelme@rfranco.com>
2024-04-21 10:19:57 +0200

    [NVIM] fix: fix nvim colorizer yet again

--- nvim/lua/plugins/ui/colorizer.lua
+++ nvim/lua/plugins/ui/colorizer.lua
@@ -1,11 +1,6 @@
 return {
   'NvChad/nvim-colorizer.lua',
   config = function()
-    require('colorizer').setup {
-      filetypes = {
-        '*',
-      },
-      user_default_options = { tailwind = true }
-    }
+    require 'colorizer'.setup{ user_default_options = { tailwind = true; } }
   end,
 }
