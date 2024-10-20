cf39b629d073f9155a6e0ae4de0e7174e6524faa
oscar <oriquelme@rfranco.com>
2024-10-20 20:26:41 +0200

    [AWESOME] feat: start awesome wm rewrite

--- awesome/.config/awesome/modules/start_up/init.lua
+++
@@ -1,6 +0,0 @@
-local awful = require("awful")
-
--- awful.spawn.with_shell("feh --bg-center ~/.config/wallpaper.png ")
-awful.util.spawn('obsidian')
-awful.util.spawn('wezterm')
-awful.util.spawn('firefox')
--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -8,8 +8,7 @@ local gears = require("gears")
 modkey = "Mod4"
 terminal = "alacritty"
 
--- local theme_dir = gears.filesystem.get_configuration_dir() .. "theme/"
--- beautiful.init(theme_dir .. "theme.lua")
+-- INIT THEME
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme/theme.lua")
 
 require("awful.autofocus")
@@ -17,11 +16,7 @@ require("awful.hotkeys_popup.keys")
 
 local bling = require("lib.bling")
 
-require("modules.start_up")
-require("modules.bar")
 require("modules.binds.bindings")
-require("modules.alt_tab")
-
 
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
@@ -43,6 +38,7 @@ awful.layout.layouts = {
 -- load globalkeys from bindings.lua
 root.keys(globalkeys)
 
+-- TODO: move this
 clientbuttons = gears.table.join(
   awful.button({ }, 1, function (c)
       c:emit_signal("request::activate", "mouse_click", {raise = true})
@@ -56,84 +52,3 @@ clientbuttons = gears.table.join(
       awful.mouse.client.resize(c)
   end)
 )
-
--- Control variables to track whether programs have been launched before or not
-local firefox_started = false
-local wezterm_started = false
-local obsidian_started = false
-local discord_started = 2
-local teams_started = 2
-
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     floating = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-                }
-    },
-
-    { rule = { class = "org.wezfurlong.wezterm"},
-    properties = { floating = false },
-    callback = function (c)
-            if not wezterm_started then
-                local s = awful.screen.focused()
-                c:move_to_screen(s)
-                c:tags({ s.tags[1] })
-
-                wezterm_started = true
-            end
-        end },
-
-    { rule = { class = "firefox"},
-    properties = { floating = false },
-    callback = function (c)
-            if not firefox_started then
-                local s = awful.screen.focused()
-                c:move_to_screen(s)
-                c:tags({ s.tags[2] })
-
-                firefox_started = true
-            end
-        end },
-
-    { rule = { class = "obsidian"},
-    callback = function (c)
-            if not obsidian_started then
-                local s = awful.screen.focused()
-                c:move_to_screen(s)
-                c:tags({ s.tags[3] })
-
-                obsidian_started = true
-            end
-        end },
-
-    { rule = { class = "Microsoft Teams - Preview"},
-    callback = function (c)
-            if teams_started ~= 0 then
-                local s = awful.screen.focused()
-                c:move_to_screen(s)
-                c:tags({ s.tags[4]})
-
-                teams_started = teams_started - 1
-            end
-        end },
-
-    { rule = { class = "discord"},
-    callback = function (c)
-            if discord_started ~= 0 then
-                local s = awful.screen.focused()
-                c:move_to_screen(s)
-                c:tags({ s.tags[5] })
-
-                discord_started = discord_started - 1
-            end
-        end },
-
-}
