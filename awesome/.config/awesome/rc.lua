a2587bf101713347358214e1e15cc2fd38ed0c4a
oscar <oscariquelmejato01@gmail.com>
2024-10-23 08:05:18 +0200

    [AWESOME] feat: implement base 16 colors and full color palette

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -41,15 +41,8 @@ require("configuration")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
--- }}}
+local theme_dir = gears.filesystem.get_configuration_dir() .. "theme/"
+beautiful.init(theme_dir .. "theme.lua")
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
--- awesome/.config/awesome/theme/theme.lua
+++ awesome/.config/awesome/theme/theme.lua
@@ -1,46 +1,66 @@
 local theme = {}
 
-theme.font = "sarasa mono k bold 13"
+theme.font = "Roboto Medium 10"
 theme.icon_font = "Material Icon Round "
-theme.font_large = "sarasa mono k bold 16"
-theme.font_extra_large = "sarasa mono k bold 20"
 
-theme.light_white   = "#fafafa"
-theme.white         = "#d7dae0"
-theme.pink          = "#e93c58"
-theme.purple        = "#ca9ee6"
-theme.red           = "#e93c58"
-theme.orange        = "#e58d7d"
-theme.yellow        = "#dfa000"
-theme.green         = "#a6d189"
-theme.light_blue    = "#99d1db"
-theme.blue          = "#a4b9ef"
-theme.text          = "#D3C6AA"
-theme.grey          = "#495156"
-theme.dark_grey     = "#374145"
-theme.darker_grey   = "#2E383C"
-theme.black         = "#0e0e0e"
-theme.deep_black    = "#000000"
+--- Special
+theme.white = "#edeff0"
+theme.darker_black = "#060809"
+theme.black = "#0c0e0f"
+theme.lighter_black = "#121415"
+theme.one_bg = "#161819"
+theme.one_bg2 = "#1f2122"
+theme.one_bg3 = "#27292a"
+theme.grey = "#343637"
+theme.grey_fg = "#3e4041"
+theme.grey_fg2 = "#484a4b"
+theme.light_grey = "#505253"
 
-theme.border_width = 1
-theme.border_normal = theme.dark_grey
+-- Black
+theme.color0 = "#1e2129"
+theme.color8 = "#27292a"
 
--- Wibar
-theme.wibar_width = 40
-theme.wibar_stretch = true
-theme.wibar_bg = theme.black
-theme.wibar_fg = theme.text
-theme.wibar_border_width = 1
-theme.wibar_border_color = theme.dark_grey
+-- Red
+theme.color1 = "#e8646a"
+theme.color9 = "#dc6b74"
 
+-- Green
+theme.color2 = "#a6d189"
+theme.color10 = "#7fc7a2"
 
-theme.taglist_font = theme.font
-theme.taglist_bg_focus = theme.black
-theme.tag_colors = {theme.red, theme.red, theme.red, theme.red, theme.red, theme.red, theme.red}
-theme.taglist_fg_focus = theme.text
+-- Yellow
+theme.color3 = "#ebcb6b"
+theme.color11 = "#e5c890"
 
-theme.useless_gap = 0
+-- Blue
+theme.color4 = "#61afef"
+theme.color12 = "#4489ce"
 
+-- Magenta
+theme.color5 = "#ea76cb"
+theme.color13 = "#f488eF"
+
+-- Cyan
+theme.color6 = "#6aa6a5"
+theme.color14 = "#81c8be"
+
+-- White
+theme.color7 = "#e4e6e7"
+theme.color15 = "#f2f4f5"
+
+theme.accent = theme.color4
+
+--- Background Colors
+theme.bg_normal = theme.black
+theme.bg_focus = theme.black
+theme.bg_urgent = theme.black
+theme.bg_minimize = theme.black
+
+--- Foreground Colors
+theme.fg_normal = theme.white
+theme.fg_focus = theme.accent
+theme.fg_urgent = theme.color1
+theme.fg_minimize = theme.color0
 
 return theme
 
