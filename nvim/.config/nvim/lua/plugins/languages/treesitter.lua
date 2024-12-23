eaf463755f981d7f8075d32ae922a25df186b105
oscar <oscariquelmejato01@gmail.com>
2024-12-23 09:30:33 +0100

    [NVIM] feat: install harpoon like the cool kids

--- nvim/.config/nvim/lua/plugins/languages/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/languages/treesitter.lua
@@ -70,15 +70,6 @@ return {
             ['[]'] = '@class.outer',
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
       },
     }
   end
